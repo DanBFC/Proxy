ba4e152f1635c0b83fbb096e76338270a016b38e
DanBFC <d_aniels@hotmail.com>
2015-05-07 19:19:29 -0500

    Clase Estudiante

---
+++ Estudiante.java
@@ -0,0 +1,14 @@
+package proxy;
+
+
+public class Estudiante{
+    public String nombre;
+    public String archivo;
+    
+    public Estudiante(String nombre, String archivo){
+        this.nombre= nombre;
+        this.archivo= archivo;
+        
+    }
+    
+}
