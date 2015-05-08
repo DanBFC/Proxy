2aadf85cde17e7c63673185a1448e0352afbdc27
DanBFC <d_aniels@hotmail.com>
2015-05-07 19:18:56 -0500

    Clase Computologo

---
+++ Computologo.java
@@ -0,0 +1,41 @@
+package proxy;
+
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class Computologo extends Estudiante{
+    
+    public Archivador proxy;
+    
+    public Computologo(String nombre, String archivo){
+        super(nombre, archivo);
+        proxy = new Archivador();
+    }
+    
+    
+    public void guardaArchivo(){
+        if(proxy.escribirArchivo(this, this.archivo)){
+            try{
+                BufferedWriter bw;
+                File archivo = new File(super.archivo);
+                if(!archivo.exists()){
+                    bw = new BufferedWriter(new FileWriter(archivo));
+                    bw.write(" ");
+                    bw.close();
+                }else{
+                    System.out.println("No se creó el archivo: " + super.archivo);
+
+                }
+                
+            }catch(Exception e){
+                System.out.println("No se creó el archivo: " + super.archivo);                
+            }
+            
+        }
+    }
+
+}
